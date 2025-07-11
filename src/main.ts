3423222a60d13bfb5b5a358445ca5019481f19bb
neveshardd <soujoseeugenio@gmail.com>
2025-07-11 09:56:07 -0300

    fix: redis

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.REDIS,
     options: {
-      host: 'redis',
+      host: process.env.REDIS_URL!,
       port: Number(process.env.REDIS_PORT) || 6379,
     }
   })
