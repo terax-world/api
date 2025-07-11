b68c82eea5a4f83c69a07feb6cd7715974a906c2
neveshardd <soujoseeugenio@gmail.com>
2025-07-11 00:59:29 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { PrismaModule } from './prisma/prisma.module';
 import { CategoryModule } from './category/category.module';
 import { ServerModule } from './server/server.module';
 import { InvoiceModule } from './invoice/invoice.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
   imports: [AppModule, ProductModule, CategoryModule, ServerModule, InvoiceModule],
-  controllers: [],
-  providers: [PrismaModule],
+  controllers: [AppController],
+  providers: [PrismaModule, AppService],
 })
 export class AppModule {}
